e747c536f80a7ebfa41867643c68e40bca9bae1b
Yury Bursian <dab180996@gmail.com>
2024-06-13 16:43:02 +0300

    Header start

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,18 @@
 // import logo from "../../logo.svg";
 import "./App.css";
 import React, { Route, Routes } from "react-router-dom";
+// import { useEffect, useState } from "react";
 import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
 import Footer from "../Footer/Footer";
 import RegisterModal from "../RegisterModal/RegisterModal";
 import LoginModal from "../LoginModal/LoginModal";
 import RegisterCompleteModal from "../RegisterCompleteModal/RegisterCompleteModal";
+import Header from "../Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <RegisterCompleteModal />
       {/* <Routes>
         <Route exact path="/" element={<Main />}></Route>
         <Route path="/saved-news" element={<SavedNews />}></Route>
