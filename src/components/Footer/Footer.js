f8706f25164782c472128f6677ce7b78b86c4207
Yury Bursian <dab180996@gmail.com>
2024-06-14 20:55:50 +0300

    Footer styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,19 @@
 import "./Footer.css";
+import GitHub from "../../images/GitHub.svg";
+import Facebook from "../../images/Facebook.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>Developed by Yury Bursian</div>
-      <div>{new Date().getFullYear()}</div>
+      <p className="footer__copyright">© 2024 Supersite, Powered by News API</p>
+      <div className="footer__buttons">
+        <button className="footer__button footer__button_text">Home</button>
+        <button className="footer__button footer__button_text">
+          TripleTen
+        </button>
+        <button className="footer__button footer__button_GH"></button>
+        <button className="footer__button footer__button_FB"></button>
+      </div>
     </footer>
   );
 };
