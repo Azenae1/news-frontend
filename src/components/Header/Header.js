b40087162ce075866f9695d7cf9f939c232ae1ec
Yury Bursian <dab180996@gmail.com>
2024-06-15 19:46:36 +0300

    Header loggedin styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../images/logo.svg";
+import logout from "../../images/logout-white.svg";
 
-const Header = () => {
+const Header1 = () => {
   return (
     <header className="header">
       <button className="header__logo-group">
@@ -16,6 +17,34 @@ const Header = () => {
 
       <button type="text" className="header__button header__button-signin">
         Sign in
+      </button>
+    </header>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="header">
+      <button className="header__logo-group">
+        <h2 className="header__logo">NewsExpolorer</h2>
+      </button>
+
+      <button type="text" className="header__button header__button-home">
+        Home
+        <div className="header__underline"></div>
+      </button>
+
+      <button type="text" className="header__button header__button-saved">
+        Saved articles
+      </button>
+
+      <button type="text" className="header__button header__button-logout">
+        <p className="header__button-text">Dude</p>
+        <img
+          src={logout}
+          className="header__logout-img"
+          alt="header-logout"
+        ></img>
       </button>
     </header>
   );
