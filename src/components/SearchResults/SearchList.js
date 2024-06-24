d0a5836f2721c7def1939999a4e6580dbe47919b
Yury Bursian <dab180996@gmail.com>
2024-06-24 21:02:07 +0300

    SearchList update

--- src/components/SearchResults/SearchList.js
+++ src/components/SearchResults/SearchList.js
@@ -1,17 +1,52 @@
-import "./SearchList.css";
+import React, { useLocation } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
 
+import "./SearchList.css";
 import NewsCard from "../NewsCard/NewsCard";
+import { getSavedNews } from "../../utils/api";
+import { SearchResultContext } from "../../contexts/SearchResultContext";
+import { CurrentPageContext } from "../../contexts/CurrentPageContext";
+import { SavedNewsContext } from "../../contexts/SavedNewsContext";
 
 const SearchList = () => {
+  const { searchResults } = useContext(SearchResultContext);
+  const { currentPage, setCurrentPage } = useContext(CurrentPageContext);
+  const { setSavedNews } = useContext(SavedNewsContext);
+
+  const location = useLocation();
+  const [visibleNews, setVisibleNews] = useState(3);
+
+  useEffect(() => {
+    setCurrentPage(location.pathname);
+  }, [location.pathname, setCurrentPage]);
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("token");
+    getSavedNews(jwt).then(setSavedNews);
+  }, [getSavedNews]);
+
+  const renderMoreNews = () => {
+    setVisibleNews((render) => render + 3);
+  };
+
   return (
     <section className="searchlist">
+      {currentPage === "/" ? (
+        <h2 className="searchlist__title">Search results</h2>
+      ) : (
+        ""
+      )}
       <h2 className="searchlist__title">Search results</h2>
       <ul className="searchlist__cards">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+        {searchResults.slice(0, visibleNews).map((news) => {
+          return <NewsCard />;
+        })}
       </ul>
-      <button className="searchlist__button">Show more</button>
+      {visibleNews < searchResults.length && (
+        <button onClick={renderMoreNews} className="searchlist__button">
+          Show more
+        </button>
+      )}
     </section>
   );
 };
