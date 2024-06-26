b0486372904c2a0f53134b626e7e6363d311d125
Yury Bursian <dab180996@gmail.com>
2024-06-26 15:01:42 +0300

    Footer links functionable

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,30 @@
+import { Link } from "react-router-dom";
+
 import "./Footer.css";
-import GitHub from "../../images/GitHub.svg";
-import Facebook from "../../images/Facebook.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer__copyright">© 2024 Supersite, Powered by News API</p>
       <div className="footer__buttons">
-        <button className="footer__button footer__button_text">Home</button>
-        <button className="footer__button footer__button_text">
-          TripleTen
-        </button>
-        <button className="footer__button footer__button_GH"></button>
-        <button className="footer__button footer__button_FB"></button>
+        <Link to="/">
+          <button className="footer__button footer__button_text">Home</button>
+        </Link>
+        <Link to="https://tripleten.com/" target="_blank" rel="noreferrer">
+          <button className="footer__button footer__button_text">
+            TripleTen
+          </button>
+        </Link>
+        <Link to="https://github.com/Azenae1" target="_blank" rel="noreferrer">
+          <button className="footer__button footer__button_GH"></button>
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/yury-bursian/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="footer__button footer__button_LI"></button>
+        </Link>
       </div>
     </footer>
   );
